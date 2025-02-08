0aba4471e20985707820205ddb62c033786f7fe4
Jonathan0x539 <jordison@protonmail.com>
2025-02-08 01:41:43 -0300

    feat(user-service): set default role to "USER" on user creation

--- src/modules/users/services/createUserService.ts
+++ src/modules/users/services/createUserService.ts
@@ -1,27 +1,31 @@
 import { Role } from "@prisma/client";
 import CreateUserRepository from "../databases/createUserRepository";
-import bcrypt from "bcryptjs";  // Importando bcryptjs
+import bcrypt from "bcryptjs";
 
 interface CreateUserInterface {
   fullName: string;
   email: string;
   password: string;
-  Role: Role;
+  Role?: Role;
 }
 
 class CreateUserService {
   async createUser(data: CreateUserInterface) {
     const { email, password } = data;
 
+    //Role opcional para USER, para admin é necessário passar a Role como ADMIN
+    const role = data.Role || Role.USER;
+
     const existingUser = await CreateUserRepository.findUserByEmail(email);
     if (existingUser && !existingUser.deletedAt) {
       throw new Error("E-mail já está em uso.");
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);  // Usando bcryptjs
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await CreateUserRepository.createUser({
       ...data,
+      Role: role,
       password: hashedPassword,
     });
 
