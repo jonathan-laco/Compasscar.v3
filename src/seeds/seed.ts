49b067b2c0e290d5589c42c466e6d9b4db683ba3
Jonathan-Laco <jordison@protonmail.com>
2025-02-16 22:58:13 -0300

    chore(seed.ts): remove comments for cleaner code

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,33 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';  // Usando bcryptjs em vez de bcrypt
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Criptografar a senha do admin
-  const hashedPassword = await bcrypt.hash('admin123', 10);  // Usando bcryptjs para hash
+  const hashedPassword = await bcrypt.hash("admin123", 10);
 
-  // Criar usuário Admin
   await prisma.user.create({
     data: {
-      fullName: 'Admin Teste',
-      email: 'admin@example.com',
+      fullName: "Admin Teste",
+      email: "admin@example.com",
       password: hashedPassword,
-      Role: 'ADMIN',
+      Role: "ADMIN",
     },
   });
-
-  /* Criar cliente
-  await prisma.client.create({
-    data: {
-      fullName: 'Cliente Teste',
-      cpf: '12345678909',
-      email: 'cliente@example.com',
-      birthDate: new Date('2000-01-01'),
-      phone: '123456789',
-    },
-  });
-  */
 }
 
 main()
